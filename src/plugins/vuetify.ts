448bf30a3b644ecb747d8b062c164eab2adf2441
Anne Lyttle <alyttle@berkeley.edu>
2023-02-03 13:40:24 -0800

    RIP-52: MUTHUR - job history

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,11 +11,12 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
-import { VDataTableVirtual } from 'vuetify/labs/VDataTable'
+import { VDataTable, VDataTableVirtual } from 'vuetify/labs/VDataTable'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   components: {
+    VDataTable,
     VDataTableVirtual
   },
   theme: {
