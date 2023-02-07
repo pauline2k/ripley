5657c359b83a6fd3713d43f8b4a92692053cb4fc
John Crossman <johncrossman@berkeley.edu>
2023-02-07 11:26:37 -0800

    RIP-68, more vue-router tweaks; and an /error page

---
+++ src/components/utils/ContactUsPrompt.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+    Problem? Question? Email us at
+    <a
+      id="ripley-support-mailto"
+      aria-label="Open new window to send email"
+      :class="hrefMailtoClass"
+      :href="`mailto:${$config.emailRipleySupport}`"
+      target="_blank"
+    >
+      {{ $config.emailRipleySupport }}
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ContactUsPrompt',
+  props: {
+    hrefMailtoClass: {
+      default: undefined,
+      required: false,
+      type: String
+    }
+  }
+}
+</script>
