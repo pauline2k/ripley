f88475822e4518632e0399d9f16001dfa354f1ef
Anne Lyttle <alyttle@berkeley.edu>
2023-03-06 14:17:12 -0800

    RIP-106: automates copying lti-config.json from S3

--- scripts/get_config_file_from_s3.sh
+++ scripts/get_config_file_from_s3.sh
@@ -2,7 +2,7 @@
 
 # -------------------------------------------------------------------
 #
-# Copy config file from S3 to production-local.py. This
+# Copy config files from S3 to production-local.py. This
 # script must be run on the target EC2 instance (eg, ripley-dev).
 #
 # -------------------------------------------------------------------
@@ -17,36 +17,43 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-local_config="/var/app/current/config/production-local.py"
+local_app_config="/var/app/current/config/production-local.py"
+local_lti_config="/var/app/current/config/lti-config.json"
 
-if [ -e "${local_config}" ]; then
-  eb_env=$(grep EB_ENVIRONMENT "${local_config}" | sed "s/^EB_ENVIRONMENT[ ]*=[ ]*'//" | sed "s/'[ ]*//")
+if [ -e "${local_app_config}" ]; then
+  eb_env=$(grep EB_ENVIRONMENT "${local_app_config}" | sed "s/^EB_ENVIRONMENT[ ]*=[ ]*'//" | sed "s/'[ ]*//")
 
   if [ -z "${eb_env}" ]; then
-    echo "[ERROR] EB_ENVIRONMENT not found in ${local_config}. Please report the problem."; echo
+    echo "[ERROR] EB_ENVIRONMENT not found in ${local_app_config}. Please report the problem."; echo
     exit 1
   else
-    echo "EB_ENVIRONMENT is '${eb_env}' according to ${local_config}"; echo
+    echo "EB_ENVIRONMENT is '${eb_env}' according to ${local_app_config}"; echo
   fi
 
 else
-  echo "File not found: ${local_config}"; echo
+  echo "File not found: ${local_app_config}"; echo
   exit 1
 fi
 
 # Download from Amazon S3
-config_location="s3://ripley-deploy-configs/ripley/${eb_env}.py"
+app_config_location="s3://ripley-deploy-configs/ripley/${eb_env}.py"
+lti_config_location="s3://ripley-deploy-configs/ripley/${eb_env}-lti-config.json"
 
-echo "In five seconds, ${config_location} will be copied to ${local_config}."; echo
+echo "In five seconds, ${app_config_location} will be copied to ${local_app_config}
+and ${lti_config_location} will be copied to ${local_lti_config}."; echo
 echo "Use CTRL-C to abort..."; echo
 sleep 5
 
-AWS_REGION=us-west-2 aws s3 cp "${config_location}" "${local_config}"
-chown webapp "${local_config}"
-chmod 400 "${local_config}"
+AWS_REGION=us-west-2 aws s3 cp "${app_config_location}" "${local_app_config}"
+chown webapp "${local_app_config}"
+chmod 400 "${local_app_config}"
+
+AWS_REGION=us-west-2 aws s3 cp "${lti_config_location}" "${local_lti_config}"
+chown webapp "${local_lti_config}"
+chmod 400 "${local_lti_config}"
 
 # Add EB_ENVIRONMENT to new config file
-printf "\nEB_ENVIRONMENT = '${eb_env}'\n\n" >> "${local_config}"
+printf "\nEB_ENVIRONMENT = '${eb_env}'\n\n" >> "${local_app_config}"
 
 echo; echo "Done!"; echo
 echo "Restart Ripley to pick up new configs. Have a nice day!"; echo
