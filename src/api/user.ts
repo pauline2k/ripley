dc7968dba94b065d328c22509324f27573d0290a
John Crossman <johncrossman@berkeley.edu>
2023-03-21 13:08:55 -0700

    RIP-114, re-authenticate when flipping to a new canvas_site_id

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,3 +3,7 @@ import utils from '@/api/api-utils'
 export function getUserProfile(uid: number) {
   return utils.post('/api/user/profile', {uid}, true)
 }
+
+export function getMyUserProfile() {
+  return utils.get('/api/user/my_profile')
+}
