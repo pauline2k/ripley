1b54a96fd267877f077c7f58916326f76524c1cd
Anne Lyttle <alyttle@berkeley.edu>
2023-06-08 12:36:38 -0700

    RIP-213: tells rq about Elasticache

--- .platform/hooks/postdeploy/02_start_rq_worker.sh
+++ .platform/hooks/postdeploy/02_start_rq_worker.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+
+# Abort immediately if a command fails
+set -e
+
+echo $(pwd)
+local_app_config="/var/app/current/config/production-local.py"
+
+if [ -e "${local_app_config}" ]; then
+  redis_host=$(sudo grep REDIS_HOST "${local_app_config}" | sed "s/^REDIS_HOST[ ]*=[ ]*'//" | sed "s/'[ ]*//")
+  redis_pw=$(sudo grep REDIS_PASSWORD "${local_app_config}" | sed "s/^REDIS_PASSWORD[ ]*=[ ]*'//" | sed "s/'[ ]*//")
+  redis_port=$(sudo grep REDIS_PORT "${local_app_config}" | sed "s/^REDIS_PORT[ ]*=[ ]*'//" | sed "s/'[ ]*//")
+
+  if [ -z "${redis_host}" ]; then
+    echo "[ERROR] REDIS_HOST not found in ${local_app_config}. Please report the problem."; echo
+    exit 1
+  elif [ -z "${redis_pw}" ]; then
+    echo "[ERROR] REDIS_PASSWORD not found in ${local_app_config}. Please report the problem."; echo
+    exit 1
+  elif [ -z "${redis_port}" ]; then
+    echo "[ERROR] REDIS_PORT not found in ${local_app_config}. Please report the problem."; echo
+    exit 1
+  else
+    echo "REDIS_HOST is '${redis_host}' according to ${local_app_config}"; echo
+  fi
+
+else
+  echo "File not found: ${local_app_config}"; echo
+  exit 1
+fi
+
 PYTHONPATH=$(/opt/elasticbeanstalk/bin/get-config environment -k PYTHONPATH)
-sudo ${PYTHONPATH}/rq worker -c xenomorph
+sudo ${PYTHONPATH}/rq worker -c xenomorph -u "rediss://default:${redis_pw}@${redis_host}:${redis_port}"
