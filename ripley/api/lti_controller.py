e263be02ab8e50edc78adaadf5d1db907fb27582
John Crossman <johncrossman@berkeley.edu>
2023-07-19 12:04:13 -0700

    RIP-252, proper LTI launch_url for mailing list

--- ripley/api/lti_controller.py
+++ ripley/api/lti_controller.py
@@ -204,7 +204,7 @@ def launch_grade_distribution():
 
 @app.route('/api/lti/mailing_list', methods=['GET', 'POST'])
 def launch_mailing_list():
-    return _launch_tool('mailing_list/manage')
+    return _launch_tool('mailing_list/select_course')
 
 
 @app.route('/api/lti/mailing_lists', methods=['GET', 'POST'])
