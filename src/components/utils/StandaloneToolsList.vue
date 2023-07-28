e8da0a5698ffccc31eabde500800d435fe0ebacd
John Crossman <johncrossman@berkeley.edu>
2023-07-27 17:00:16 -0700

    RIP-252, add clarity to /welcome page

---
+++ src/components/utils/StandaloneToolsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-list density="compact" :lines="false">
+    <template v-for="(tool, index) in tools" :key="index">
+      <v-list-item>
+        <template #prepend>
+          <v-icon class="mr-4" :icon="tool.icon" />
+        </template>
+        <v-list-item-title>
+          <span v-if="tool.disabled" class="font-weight-light">
+            {{ tool.title }}
+          </span>
+          <router-link
+            v-if="!tool.disabled"
+            :id="getLinkId(tool)"
+            class="text-decoration-none"
+            :to="tool.path"
+          >
+            {{ tool.title }}
+          </router-link>
+        </v-list-item-title>
+      </v-list-item>
+    </template>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'StandaloneToolsList',
+  props: {
+    tools: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    getLinkId(tool) {
+      return `tool-${tool.title.toLowerCase().replace(/[ ]+/g, '-')}-link`
+    }
+  }
+}
+</script>
