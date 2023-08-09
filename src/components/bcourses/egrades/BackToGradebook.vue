47f8fe5b19b9536d5dd885a05b99cc662ad3b73f
John Crossman <johncrossman@berkeley.edu>
2023-08-09 16:27:24 -0700

    RIP-310, UI work on step in 'Export E-Grades' flow

---
+++ src/components/bcourses/egrades/BackToGradebook.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pr-1">
+    <v-icon icon="mdi-chevron-left" />
+  </div>
+  <div>
+    <a class="template-back-link" :href="`${config.canvasApiUrl}/courses/${currentUser.canvasSiteId}/grades`" target="_top">Back to Gradebook</a>
+  </div>
+</template>
+
+<script>
+import Context from '@/mixins/Context.vue'
+
+export default {
+  name: 'BackToGradebook',
+  mixins: [Context]
+}
+</script>
