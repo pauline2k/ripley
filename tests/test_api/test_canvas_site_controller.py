d6cdb93bfab937b2c70fafc646dfb9b6d9dddcf7
John Crossman <johncrossman@berkeley.edu>
2023-10-13 12:43:03 -0700

    RIP-487, pytest coverage for /api/canvas_site/manage_official_sections

--- tests/test_api/test_canvas_site_controller.py
+++ tests/test_api/test_canvas_site_controller.py
@@ -28,6 +28,9 @@ import json
 import requests_mock
 from tests.util import register_canvas_uris
 
+TERM_ID_CURRENT = '2232'
+TERM_ID_NEXT = '2235'
+
 admin_uid = '10000'
 faculty_uid = '90000'
 no_canvas_account_uid = '10001'
@@ -38,11 +41,113 @@ ta_uid = '50000'
 teacher_uid = '30000'
 
 
-class TestCanvasSiteEditSections:
+class TestViewOfficialSections:
+
+    EMPTY_OFFICIAL_SECTIONS_FEED = {TERM_ID_CURRENT: [], TERM_ID_NEXT: []}
+
+    @classmethod
+    def _api_canvas_site_official_sections(cls, client, expected_status_code=200):
+        response = client.get('/api/canvas_site/manage_official_sections')
+        assert response.status_code == expected_status_code
+        return response.json
 
     def test_anonymous(self, client):
         """Denies anonymous user."""
-        _api_canvas_site_edit_sections(client, '8876542', expected_status_code=401)
+        self._api_canvas_site_official_sections(client, expected_status_code=401)
+
+    def test_no_canvas_account(self, client, app, fake_auth):
+        """Denies user with no Canvas account."""
+        with requests_mock.Mocker() as m:
+            canvas_site_id = '8876542'
+            register_canvas_uris(app, {'course': [f'get_by_id_{canvas_site_id}']}, m)
+            fake_auth.login(canvas_site_id=canvas_site_id, uid=no_canvas_account_uid)
+            self._api_canvas_site_official_sections(client, expected_status_code=401)
+
+    def test_reader(self, client, app, fake_auth):
+        """Official sections view is not available to Reader role."""
+        with requests_mock.Mocker() as m:
+            canvas_site_id = '8876542'
+            uid = reader_uid
+            register_canvas_uris(app, {
+                'account': ['get_admins', 'get_by_id'],
+                'course': [
+                    f'get_by_id_{canvas_site_id}',
+                    f'get_enrollments_{canvas_site_id}_7890123',
+                    f'get_sections_{canvas_site_id}',
+                ],
+                'user': [f'profile_{uid}'],
+            }, m)
+            fake_auth.login(canvas_site_id=canvas_site_id, uid=uid)
+            assert self._api_canvas_site_official_sections(client) == self.EMPTY_OFFICIAL_SECTIONS_FEED
+
+    def test_teaching_roles(self, client, app, fake_auth):
+        """Official sections view is available teaching roles."""
+        with requests_mock.Mocker() as m:
+            canvas_user_ids = {
+                ta_uid: 6789012,
+                teacher_uid: 4567890,
+            }
+            for uid in [ta_uid, teacher_uid]:
+                canvas_site_id = 8876542
+                register_canvas_uris(app, {
+                    'account': ['get_admins', 'get_by_id'],
+                    'course': [
+                        f'get_by_id_{canvas_site_id}',
+                        f'get_enrollments_{canvas_site_id}_{canvas_user_ids[uid]}',
+                        f'get_sections_{canvas_site_id}',
+                    ],
+                    'user': [
+                        f'profile_{uid}',
+                        f'user_courses_{uid}',
+                    ],
+                }, m)
+                fake_auth.login(canvas_site_id=canvas_site_id, uid=uid)
+                api_json = self._api_canvas_site_official_sections(client)
+                assert TERM_ID_CURRENT in api_json
+                current_term_courses = api_json[TERM_ID_CURRENT]
+                assert len(current_term_courses) == 1
+                assert current_term_courses[0]['canvasSiteId'] == canvas_site_id
+
+    def test_teacher_no_section_ids(self, client, app, fake_auth):
+        """Teacher without sections gets empty feed."""
+        with requests_mock.Mocker() as m:
+            uid = teacher_uid
+            canvas_site_id = '8876542'
+            register_canvas_uris(app, {
+                'account': ['get_admins'],
+                'course': [
+                    f'get_by_id_{canvas_site_id}',
+                    f'get_sections_{canvas_site_id}',
+                    f'get_enrollments_{canvas_site_id}_4567890',
+                ],
+                'user': [f'profile_{uid}'],
+            }, m)
+            fake_auth.login(canvas_site_id=canvas_site_id, uid=uid)
+            sections = self._api_canvas_site_official_sections(client)
+            assert sections == self.EMPTY_OFFICIAL_SECTIONS_FEED
+
+
+class TestEditOfficialSections:
+
+    @classmethod
+    def _api_edit_official_sections(cls, client, canvas_site_id, params=None, expected_status_code=200):
+        data = {
+            'sectionIdsToAdd': [],
+            'sectionIdsToRemove': [],
+            'sectionIdsToUpdate': [],
+        }
+        data.update(params or {})
+        response = client.post(
+            f'/api/canvas_site/{canvas_site_id}/provision/sections',
+            data=json.dumps(data),
+            content_type='application/json',
+        )
+        assert response.status_code == expected_status_code
+        return response.json
+
+    def test_anonymous(self, client):
+        """Denies anonymous user."""
+        self._api_edit_official_sections(client, '8876542', expected_status_code=401)
 
     def test_no_canvas_account(self, client, app, fake_auth):
         """Denies user with no Canvas account."""
@@ -50,7 +155,7 @@ class TestCanvasSiteEditSections:
             register_canvas_uris(app, {'course': ['get_by_id_8876542']}, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=no_canvas_account_uid)
-            _api_canvas_site_edit_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_edit_official_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_reader(self, client, app, fake_auth):
         """Denies Reader."""
@@ -62,7 +167,7 @@ class TestCanvasSiteEditSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=reader_uid)
-            _api_canvas_site_edit_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_edit_official_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_ta(self, client, app, fake_auth):
         """Denies TA."""
@@ -74,7 +179,7 @@ class TestCanvasSiteEditSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=ta_uid)
-            _api_canvas_site_edit_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_edit_official_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_teacher(self, client, app, fake_auth):
         """Allows teacher."""
@@ -89,7 +194,7 @@ class TestCanvasSiteEditSections:
                 'sectionIdsToAdd': ['12345'],
             }
             fake_auth.login(canvas_site_id=canvas_site_id, uid=teacher_uid)
-            _api_canvas_site_edit_sections(client, canvas_site_id, params=params)
+            self._api_edit_official_sections(client, canvas_site_id, params=params)
 
     def test_teacher_no_section_ids(self, client, app, fake_auth):
         with requests_mock.Mocker() as m:
@@ -100,30 +205,23 @@ class TestCanvasSiteEditSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=teacher_uid)
-            _api_canvas_site_edit_sections(client, canvas_site_id, expected_status_code=400)
-
-
-def _api_canvas_site_edit_sections(client, canvas_site_id, params=None, expected_status_code=200):
-    data = {
-        'sectionIdsToAdd': [],
-        'sectionIdsToRemove': [],
-        'sectionIdsToUpdate': [],
-    }
-    data.update(params or {})
-    response = client.post(
-        f'/api/canvas_site/{canvas_site_id}/provision/sections',
-        data=json.dumps(data),
-        content_type='application/json',
-    )
-    assert response.status_code == expected_status_code
-    return response.json
+            self._api_edit_official_sections(client, canvas_site_id, expected_status_code=400)
 
 
 class TestCanvasSiteProvision:
 
+    @classmethod
+    def _api_canvas_course_provision(cls, client, params=None, expected_status_code=200):
+        path = '/api/canvas_site/provision'
+        if params:
+            path += f'?{"&".join([k + "=" + v for k, v in params.items()])}'
+        response = client.get(path)
+        assert response.status_code == expected_status_code
+        return response.json
+
     def test_anonymous(self, client):
         """Denies anonymous user."""
-        _api_canvas_course_provision(client, expected_status_code=401)
+        self._api_canvas_course_provision(client, expected_status_code=401)
 
     def test_teacher(self, client, app, fake_auth):
         with requests_mock.Mocker() as m:
@@ -133,7 +231,7 @@ class TestCanvasSiteProvision:
                 'user': ['profile_30000'],
             }, m)
             fake_auth.login(canvas_site_id=None, uid=teacher_uid)
-            feed = _api_canvas_course_provision(client)
+            feed = self._api_canvas_course_provision(client)
             assert feed['teachingTerms'][0]['name'] == 'Spring 2023'
             assert feed['teachingTerms'][0]['classes'][0]['courseCode'] == 'ANTHRO 189'
 
@@ -145,7 +243,7 @@ class TestCanvasSiteProvision:
                 'user': ['profile_30000', f'profile_{admin_uid}'],
             }, m)
             fake_auth.login(canvas_site_id=None, uid=admin_uid)
-            feed = _api_canvas_course_provision(client, params={'adminActingAs': teacher_uid, 'adminTermSlug': 'spring-2023'})
+            feed = self._api_canvas_course_provision(client, params={'adminActingAs': teacher_uid, 'adminTermSlug': 'spring-2023'})
             assert feed['teachingTerms'][0]['name'] == 'Spring 2023'
             assert feed['teachingTerms'][0]['classes'][0]['courseCode'] == 'ANTHRO 189'
 
@@ -157,24 +255,22 @@ class TestCanvasSiteProvision:
                 'user': ['profile_30000', f'profile_{admin_uid}'],
             }, m)
             fake_auth.login(canvas_site_id=None, uid=admin_uid)
-            feed = _api_canvas_course_provision(client, params={'adminBySectionIds[]': '32936', 'adminTermSlug': 'spring-2023'})
+            feed = self._api_canvas_course_provision(client, params={'adminBySectionIds[]': '32936', 'adminTermSlug': 'spring-2023'})
             assert feed['teachingTerms'][0]['name'] == 'Spring 2023'
             assert feed['teachingTerms'][0]['classes'][0]['courseCode'] == 'ANTHRO 189'
 
 
-def _api_canvas_course_provision(client, params=None, expected_status_code=200):
-    path = '/api/canvas_site/provision'
-    if params:
-        path += f'?{"&".join([k + "=" + v for k, v in params.items()])}'
-    response = client.get(path)
-    assert response.status_code == expected_status_code
-    return response.json
-
-
 class TestCanvasSiteProvisionSections:
+
+    @classmethod
+    def _api_canvas_course_provision_sections(cls, client, canvas_site_id, expected_status_code=200):
+        response = client.get(f'/api/canvas_site/{canvas_site_id}/official_sections')
+        assert response.status_code == expected_status_code
+        return response.json
+
     def test_anonymous(self, client):
         """Denies anonymous user."""
-        _api_canvas_course_provision_sections(client, '8876542', expected_status_code=401)
+        self._api_canvas_course_provision_sections(client, '8876542', expected_status_code=401)
 
     def test_no_canvas_account(self, client, app, fake_auth):
         """Denies user with no Canvas account."""
@@ -182,7 +278,7 @@ class TestCanvasSiteProvisionSections:
             register_canvas_uris(app, {'course': ['get_by_id_8876542']}, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=no_canvas_account_uid)
-            _api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_not_enrolled(self, client, app, fake_auth):
         """Denies user with no course enrollment."""
@@ -190,7 +286,7 @@ class TestCanvasSiteProvisionSections:
             register_canvas_uris(app, {'course': ['get_by_id_8876542'], 'user': ['profile_20000']}, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=not_enrolled_uid)
-            _api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_student(self, client, app, fake_auth):
         """Denies user with no course enrollment."""
@@ -198,7 +294,7 @@ class TestCanvasSiteProvisionSections:
             register_canvas_uris(app, {'course': ['get_by_id_8876542'], 'user': [f'profile_{student_uid}']}, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=student_uid)
-            _api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_reader(self, client, app, fake_auth):
         """Denies Reader."""
@@ -210,7 +306,7 @@ class TestCanvasSiteProvisionSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=reader_uid)
-            _api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
+            self._api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=401)
 
     def test_ta(self, client, app, fake_auth):
         """Allows TA, read-only."""
@@ -222,7 +318,7 @@ class TestCanvasSiteProvisionSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=ta_uid)
-            response = _api_canvas_course_provision_sections(client, canvas_site_id)
+            response = self._api_canvas_course_provision_sections(client, canvas_site_id)
             assert response['canvasSite']
             assert response['canvasSite']['canEdit'] is False
             assert response['canvasSite']['term'] == {
@@ -250,7 +346,7 @@ class TestCanvasSiteProvisionSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=teacher_uid)
-            response = _api_canvas_course_provision_sections(client, canvas_site_id)
+            response = self._api_canvas_course_provision_sections(client, canvas_site_id)
             assert response['canvasSite']
             assert response['canvasSite']['canEdit'] is True
             assert response['canvasSite']['term'] == {
@@ -369,7 +465,7 @@ class TestCanvasSiteProvisionSections:
             }, m)
             canvas_site_id = '8876542'
             fake_auth.login(canvas_site_id=canvas_site_id, uid=admin_uid)
-            response = _api_canvas_course_provision_sections(client, canvas_site_id)
+            response = self._api_canvas_course_provision_sections(client, canvas_site_id)
             assert response['canvasSite']
             assert response['canvasSite']['canEdit'] is False
             # Official sections
@@ -381,12 +477,6 @@ class TestCanvasSiteProvisionSections:
             assert len(spring_term['classes']) == 1
             assert spring_term['classes'][0]
             assert spring_term['classes'][0]['slug'] == 'anthro-189-2023-B'
-
-
-def _api_canvas_course_provision_sections(client, canvas_site_id, expected_status_code=200):
-    response = client.get(f'/api/canvas_site/{canvas_site_id}/official_sections')
-    assert response.status_code == expected_status_code
-    return response.json
 
 
 class TestCreateCourseSite:
