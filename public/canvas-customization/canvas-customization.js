d321e11b440655b2a12b9a4d0ffb3449dfe850af
Miles Lincoln <milesl@berkeley.edu>
2025-06-16 14:06:26 -0700

    RIP-898 one more langauge tweak

--- public/canvas-customization/canvas-customization.js
+++ public/canvas-customization/canvas-customization.js
@@ -563,7 +563,7 @@ addMentalHealthResourcesResponsiveLink();
       var altMediaPanel = [
         '<div id="alt-media-container" class="alert alert-info">',
         '  <button class="btn-link element_toggler" aria-controls="alt-media-content" aria-expanded="false" aria-label="Notice to Instructors for Making Course Materials Accessible">',
-        '    <i class="icon-arrow-right"></i> <strong>Instructors: Making Course Materials Accessible (click to expand)</strong>',
+        '    <i class="icon-arrow-right"></i> <strong>Instructors: Making Course Materials Accessible (click/select to expand)</strong>',
         '  </button>',
         '  <div id="alt-media-content" class="hide" role="region" tabindex="-1">',
         '    <div>Without the assistance of course instructors, the University cannot fulfill its mission and ' +
