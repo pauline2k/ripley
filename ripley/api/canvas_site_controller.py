eda0e1009d2d3b4f2fa9c9d9d436ef913c3b1609
Pauline Kerschen <kerschen@berkeley.edu>
2026-07-14 13:37:50 -0700

    RIP-1106 Include completed courses in archival status feed

--- ripley/api/canvas_site_controller.py
+++ ripley/api/canvas_site_controller.py
@@ -238,11 +238,10 @@ def get_provision_status():
 @app.route('/api/canvas_site/archival_status')
 @login_required
 def get_all_archival_statuses():
-    canvas_courses = canvas.get_user_courses(current_user.uid)
+    canvas_courses = canvas.get_user_courses(current_user.uid, include_completed=True)
     candidate_sites_by_id = {}
     for canvas_course in canvas_courses:
-        sis_term_id = canvas_course.term.get('sis_term_id')
-        if sis_term_id and sis_term_id.startswith('TERM:'):
+        if hasattr(canvas_course, 'enrollments'):
             enrollments = list(filter(lambda e: e.get('user_id') == current_user.canvas_user_id, canvas_course.enrollments))
             current_user_roles = [e['role'] for e in enrollments]
             matching_role = next((role for role in current_user_roles if role in ROLES_CAN_VIEW_ARCHIVAL_STATUS), None)
